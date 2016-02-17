dff6945efec766a8db1dcf728cf781cf37a831a7
Radu Bogoevici <radu.bogoevici@appmospheres.com>
2016-02-17 10:51:44 +0200

    Make the gem compatible with Refinery 3

--- lib/generators/refinery_member_pages/templates/config/page_decorator.rb
+++
@@ -1,5 +0,0 @@
-Refinery::Page.class_eval do
-
-  attr_accessible :members_only
-
-end
--- lib/generators/refinery_member_pages/templates/config/pages_controller_decorator.rb
+++ lib/generators/refinery_member_pages/templates/config/pages_controller_decorator.rb
@@ -1,15 +1,13 @@
 Refinery::PagesController.class_eval do
-
   before_filter :check_members_only
 
   protected
   def check_members_only
-    if page.members_only? && !current_refinery_user
-      flash[:notice] = t('login_required', :scope => 'notice')
+    if page.members_only? && !signed_in?
+      flash[:notice] = t('login_required', scope: 'notice')
       redirect_to root_url
       false
     end
     true
   end
-
 end

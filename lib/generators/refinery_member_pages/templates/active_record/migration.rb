d92acaa706e0b13726c9bfab9afea29cfb02fdb8
Radu Bogoevici <radu.bogoevici@appmospheres.com>
2017-10-02 15:05:51 +0300

    Update for Rails 5

--- lib/generators/refinery_member_pages/templates/active_record/migration.rb
+++ lib/generators/refinery_member_pages/templates/active_record/migration.rb
@@ -1,4 +1,4 @@
-class AddMembersOnlyToRefineryPages < ActiveRecord::Migration
+class AddMembersOnlyToRefineryPages < ActiveRecord::Migration[4.2]
   def change
     add_column :refinery_pages, :members_only, :boolean, default: false
   end
--- lib/refinery_member_pages/version.rb
+++ lib/refinery_member_pages/version.rb
@@ -1,5 +1,5 @@
 module Refinery
   module MemberPages
-    VERSION = '0.2'
+    VERSION = '0.3'
   end
 end
